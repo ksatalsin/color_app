7426d49c166fc653f4ea5babf6c3e56f6c2c50fc
Stan <ksatalsin@gmail.com>
2023-11-07 10:16:44 +0100

    build: gh flow

---
+++ lib/main_screen/view/tes_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TestVidget extends StatefulWidget {
+  const TestVidget({super.key});
+
+  @override
+  State<TestVidget> createState() => _TestVidgetState();
+}
+
+class _TestVidgetState extends State<TestVidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
